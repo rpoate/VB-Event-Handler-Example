55e605239b40f02b4798f63e6068132d5219a5f4
Richard Poate <info@zoople.com.au>
2025-06-28 11:17:03 +1000

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports Zoople
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        AddHandler Me.HtmlEditControl1.CancellableUserInteraction, AddressOf CancellableUserInteraction
+        Dim oButton = Me.HtmlEditControl1.ToolStripItems.Add("Code Wrap")
+        AddHandler oButton.Click, AddressOf CodeWrap_Click
+    End Sub
+
+    Private Sub CodeWrap_Click(sender As Object, e As EventArgs)
+        Me.HtmlEditControl1.CodeEditor.WordWrap = Not Me.HtmlEditControl1.CodeEditor.WordWrap
+    End Sub
+
+    Private Sub CancellableUserInteraction(sender As Object, e As CancellableUserInteractionEventsArgs)
+
+        If e.InteractionType = Zoople.EditorUIEvents.onkeydown Then
+
+            If e.Keys.Keycode = Keys.Tab Then
+
+                e.Cancel = True
+                HtmlEditControl1.InsertAtCursor("&nbsp;&nbsp;", Zoople.HTMLEditControl.ed_InsertType.ed_InsertReplaceSelection)
+                ' HtmlEditControl1.InsertHTMLElement("span").InnerHtml = "&nbsp;&nbsp;"
+            End If
+        End If
+
+    End Sub
+End Class
